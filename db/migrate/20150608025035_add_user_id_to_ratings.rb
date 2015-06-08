8ae6eec1a8b384a61a1b7c5f2d33899f65b328a5
Sergei O. Udalov <sergei.udalov@gmail.com>
2015-06-08 17:51:15 +0300

    delete all ratings before adding users

--- db/migrate/20150608025035_add_user_id_to_ratings.rb
+++ db/migrate/20150608025035_add_user_id_to_ratings.rb
@@ -1,5 +1,6 @@
 class AddUserIdToRatings < ActiveRecord::Migration
   def change
+    Rating.delete_all
     add_column :ratings, :user_id, :integer, null: false
     add_index :ratings, :user_id
   end
